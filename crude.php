ed895607ce238e4dffc81cb7f7bf178a0ee9cfe5
GAbizuto <gabrielbrach9555@gmail.com>
2023-05-31 10:45:55 -0300

    safdbashf

--- crude.php
+++ crude.php
@@ -1,12 +1,11 @@
 <?php
     include("conecta.php");
 
-    $nome  = $_POST["nome"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
     if(isset($_POST["inserir"])) {
-        $comando = $pdo->prepare("INSERT INTO usuario_data (nome, email, senha) VALUES('$nome', '$email', '$senha')");
+        $comando = $pdo->prepare("INSERT INTO (login_trilermarine) ( email, senha) VALUES('$email', '$senha')");
         $resultado = $comando->execute();
         header("Location: cadastrofoda.html");
     }
