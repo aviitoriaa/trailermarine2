b0e9a07b247afdcc25d94cb3a72e9cafd990dd80
GAbizuto <Gabrielbrach9555@gmail.com>
2023-06-07 12:18:58 -0300

    caralhooooooo

--- crude.php
+++ crude.php
@@ -3,9 +3,11 @@
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
+    $telefone = $_POST["telefone"];
+    $cpf =$_POST["cpf"];
 
     if(isset($_POST["inserir"])) {
-        $comando = $pdo->prepare("INSERT INTO usuario_data (nome, email, senha) VALUES('$nome', '$email', '$senha')");
+        $comando = $pdo->prepare("INSERT INTO usuario_data (nome, email, senha, telefone, cpf) VALUES('$nome', '$email', '$senha','$telefone','$cpf')");
 
         $resultado = $comando->execute();
         header("Location: login.html");
--- verificar.php
+++ verificar.php
@@ -1,17 +1,28 @@
 <?php
-
+  session_start();
   $email = $_POST ["email"]; //pega o input
   $senha = $_POST ["senha"];
 
     include("conecta.php"); // conectar com banco de dados
 
-    $comando = $pdo->prepare("SELECT * FROM  usuario_data WHERE email='$email' and senha='$senha' ");
+    $comando = $pdo->prepare("SELECT * FROM  usuario_data WHERE email = :email and senha = :senha ");
+    $comando->bindParam(':email', $email);
+    $comando->bindParam(':senha', $senha);
     $resultado = $comando->execute();
+
+    if($resultado === TRUE)
+    {
+
+    }
+
     $n = 0;
     $admin= "n";
     while ( $linhas = $comando->fetch() )
     {
         $n = 1;
+        $nome = $linhas["nome"];
+        $telefone= $linhas["telefone"];
+        $cpf = $linhas["cpf"];
         $admin = $linhas ["admin"];
     }
 
@@ -30,6 +41,11 @@
         else
         {
             header ("Location: paginainicial.html");
+            $_SESSION['email'] = $email;
+            $_SESSION['nome'] = $nome;
+            $_SESSION['senha'] = $senha;
+            $_SESSION['telefone'] = $telefone;
+            $_SESSION['cpf'] = $cpf;
         }
     }
 
