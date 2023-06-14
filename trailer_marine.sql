2a2d56eb75c0663bc0ac28b66adbfd06f1b69c0b
joaofilipigtissi <joaofilipigtissi@gmail.com>
2023-06-14 12:42:21 -0300

    sd

--- trailer_marine.sql
+++ trailer_marine.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 31/05/2023 às 22:46
+-- Tempo de geração: 14-Jun-2023 às 17:31
 -- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,42 +24,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuario_data`
+-- Estrutura da tabela `usuario_data`
 --
 
 CREATE TABLE `usuario_data` (
   `id_usuario` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `senha` varchar(45) NOT NULL
+  `senha` varchar(45) NOT NULL,
+  `telefone` varchar(11) NOT NULL,
+  `cpf` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `usuario_data`
+-- Extraindo dados da tabela `usuario_data`
 --
 
-INSERT INTO `usuario_data` (`id_usuario`, `nome`, `email`, `senha`) VALUES
-(1, ' gabriel', 'Gabrielbrach9555@gmail.com', 'adasd');
+INSERT INTO `usuario_data` (`id_usuario`, `nome`, `email`, `senha`, `telefone`, `cpf`) VALUES
+(1, ' gabriel', 'Gabrielbrach9555@gmail.com', 'adasd', '', ''),
+(2, 'joao', 'joao@gmail', 'jfgt2005', '47999999999', '12345678910');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `usuario_data`
+-- Índices para tabela `usuario_data`
 --
 ALTER TABLE `usuario_data`
   ADD PRIMARY KEY (`id_usuario`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `usuario_data`
 --
 ALTER TABLE `usuario_data`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
